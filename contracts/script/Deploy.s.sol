fcecd529a6e4aecaf7ab371a7c0c9025d862f9a3
eugenioclrc <eugenioclrc@gmail.com>
2025-08-10 01:24:23 -0300

    .

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// local deploy:
+// forge script script/Deploy.s.sol --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --rpc-url http://127.0.0.1:8545 --broadcast
+
 import {Script} from "forge-std/Script.sol";
 import {MafiaCaster} from "../src/MafiaCaster.sol";
 import {console} from "forge-std/console.sol";
@@ -19,6 +22,26 @@ contract DeployScript is Script {
 		);
 
 		console.log("MafiaCaster deployed at:", address(mafiaCaster));
+
+        MafiaCaster.Mission memory mission = MafiaCaster.Mission({
+            enable: true,
+            energyConsume: 1,
+            winOdds: 0.7 ether,
+            reqAmount: 0,
+            reqIds1155: new uint256[](0),
+            reqAmounts1155: new uint256[](0),
+            rewAmount: 4 ether,
+            rewIds1155: new uint256[](0),
+            rewAmounts1155: new uint256[](0)
+        });
+        mafiaCaster.addMission(mission);
+
+        payable(0xB182fEEad77A88B30611E3Aa5847F148d81768b3).transfer(1 ether);
+
+        mafiaCaster.doMission(1);
+
+        console.log(address(mafiaCaster.money()));
+        console.log(address(mafiaCaster.items()));
 
 		vm.stopBroadcast();
 	}
